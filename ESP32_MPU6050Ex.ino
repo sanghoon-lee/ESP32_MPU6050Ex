97b220b248802ae2d4ac83b0cc8b9b1e906a8424
sanghoon.lee@aerix.co.kr <sanghoon.lee@aerix.co.kr>
2023-03-06 17:09:12 +0900

    2023년3월6일 Release

--- ESP32_MPU6050Ex.ino
+++ ESP32_MPU6050Ex.ino
@@ -2,9 +2,9 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-const char* ssid  = "sanghoon's a52s"  // 연결할 와이파이 공유기(AP)의 SSID명칭
-const char* password = "w2f2password";  // 연결한 와이파이 공유기(AP)의 비밀번호
-const char* server_ip = "192.168.20.87";  // MQTT Broker의 접속주소
+const char* ssid  = "Aerix-Office";  // 연결할 와이파이 공유기(AP)의 SSID명칭
+const char* password = "@erix61464";  // 연결한 와이파이 공유기(AP)의 비밀번호
+const char* server_ip = "192.168.0.215";  // MQTT Broker의 접속주소
 const int server_port = 1883;           // MQTT Broker 접속 포트번호
 
 WiFiClient wifi_client;
@@ -39,6 +39,7 @@ const uint8_t GYRO_YOUT_L = 0x46;
 const uint8_t GYRO_ZOUT_H = 0x47;
 const uint8_t GYRO_ZOUT_L = 0x48;
 const uint8_t PWR_MGMT_1 = 0x6B;
+const uint8_t WHO_AM_I = 0x75;
 
 /** 
   MPU6050의 센서 데이터 구조체 선언
@@ -47,10 +48,10 @@ typedef struct _MPU6050Data{
   int16_t AcX; // X축 가속도(2바이트)
   int16_t AcY; // Y축 가속도(2바이트)
   int16_t AcZ; // Z축 가속도(2바이트)
-  int16_t Temp; // 온도(2바이트)
+  /**int16_t Temp; // 온도(2바이트)
   int16_t GyX; // X축 자이로(2바이트)
   int16_t GyY; // Y축 자이로(2바이트)
-  int16_t GyZ; // Z축 자이로(2바이트)
+  int16_t GyZ; // Z축 자이로(2바이트)*/
 } MPU6050Data;
 
 /**
@@ -104,10 +105,19 @@ void mqtt_connect(){
   MPU6050의 동작을 활성화 시키는 함수 (wakeup)
 */
 void wakeup(){
-  Wire.begin();
   Wire.beginTransmission(MPU6050_I2C_ADDR); // I2C통신으로 데이터 전송 시작 (MPU6050)
   Wire.write(PWR_MGMT_1); // 레지스터 위치를 PWR_MGMT_1로 지정
   Wire.write(0x00); // 0x00을 현재 레지스터(즉 PWR_MGMT_1 레지스터)에 기록
+  Wire.endTransmission(true); // I2C통신으로 데이터 전송 종료 (연결 해제)
+}
+
+/**
+  MPU6050을 리셋 시키는 함수 (reset)
+*/
+void reset(){
+  Wire.beginTransmission(MPU6050_I2C_ADDR); // I2C통신으로 데이터 전송 시작 (MPU6050)
+  Wire.write(PWR_MGMT_1); // 레지스터 위치를 PWR_MGMT_1로 지정
+  Wire.write(0x80); // 0x80을 현재 레지스터(즉 PWR_MGMT_1 레지스터)에 기록
   Wire.endTransmission(true); // I2C통신으로 데이터 전송 종료 (연결 해제)
 }
 
@@ -157,15 +167,50 @@ void measure() {
   Wire.endTransmission(false); // I2C통신으로 데이터 전송 종료 (연결 유지)
   // I2C통신으로 MPU6050의 현재 레지스터 위치로부터 14바이트의 데이터 수신 
   // (수신 완료 후 연결 종료)
-  Wire.requestFrom(MPU6050_I2C_ADDR,(size_t)14,true); 
-  sensingValue.AcX = Wire.read() << 8 | Wire.read();
-  sensingValue.AcY = Wire.read() << 8 | Wire.read();
-  sensingValue.AcZ = Wire.read() << 8 | Wire.read();
-  sensingValue.Temp = Wire.read() << 8 | Wire.read();
-  sensingValue.GyX = Wire.read() << 8 | Wire.read();
-  sensingValue.GyY = Wire.read() << 8 | Wire.read();
-  sensingValue.GyZ = Wire.read() << 8 | Wire.read();
+  uint8_t receivedBytes = Wire.requestFrom(MPU6050_I2C_ADDR,(size_t)6,true);
+  /**if(receivedBytes!=6){
+    sensingValue.AcX = 0xFFFF;
+    sensingValue.AcY =0xFFFF;
+    sensingValue.AcZ = 0xFFFF;
+  } else {
+    sensingValue.AcX = Wire.read() << 8 | Wire.read();
+    sensingValue.AcY = Wire.read() << 8 | Wire.read();
+    sensingValue.AcZ = Wire.read() << 8 | Wire.read();
+  }*/
+  //while(!Wire.available());
+  
+  int n = Wire.requestFrom(MPU6050_I2C_ADDR,(size_t)6,true);
+  if(n==6){
+    sensingValue.AcX = Wire.read() << 8 | Wire.read();
+    sensingValue.AcY = Wire.read() << 8 | Wire.read();
+    sensingValue.AcZ = Wire.read() << 8 | Wire.read();
+  } /**else{
+    mqtt_client.publish("mpu6050","reset=============================>");
+    reset();
+    delay(100);
+    ESP.restart();
+  }*/
+  
+  delay(5);
 }
+
+/**
+  WhoAmI 레지트터 값 확인(MPU6050 정상연결 체크)
+*/
+int8_t whoAmI(){
+  int8_t response = 0xFF;
+  Wire.beginTransmission(MPU6050_I2C_ADDR); // I2C통신으로 데이터 전송 시작 (MPU6050)
+  Wire.write(WHO_AM_I); // 레지스터 위치를 WHO_AM_I로 지정
+  Wire.endTransmission(false); // I2C통신으로 데이터 전송 종료 (연결 유지)
+
+  int n = Wire.requestFrom(MPU6050_I2C_ADDR,(size_t)1,true);
+  if(n==1){
+    response = Wire.read();
+  }
+
+  return response;  
+}
+
 
 /**
   MPU6050의 가속도 센서 값 Callibration
@@ -182,7 +227,6 @@ void callibration(){
     AcX += sensingValue.AcX;
     AcY += sensingValue.AcY;
     AcZ += sensingValue.AcZ;
-    delay(10);
   }
 
   callibrationValue.AcX = AcX / counts;
@@ -197,10 +241,12 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200); // 시리얼 통신 시작 (baudrate는 115200으로 설정)
 
+  Wire.begin();
   wakeup();
   setFullScaleRange();
   callibration();
   G_UNIT = getGUnit();
+  //sleep();
 }
 
 /**
@@ -208,24 +254,9 @@ void setup() {
 */
 void loop() {
   // put your main code here, to run repeatedly:
-  measure();
+  char msg[80];
+  memset(msg,0x00,sizeof(msg));
   
-  // callibration값에서 측정된 가속도 값을 차감
-  sensingValue.AcX = (sensingValue.AcX-callibrationValue.AcX);
-  sensingValue.AcY = (sensingValue.AcY-callibrationValue.AcY);
-  sensingValue.AcZ = (sensingValue.AcZ-callibrationValue.AcZ);
-
-  /**
-  Serial.printf("ACCEL_X : %d\n",sensingValue.AcX);
-  Serial.printf("ACCEL_Y : %d\n",sensingValue.AcY);
-  Serial.printf("ACCEL_Z : %d\n",sensingValue.AcZ);
-  Serial.printf("TEMP : %f\n",(sensingValue.Temp)/340.00+36.53);
-  Serial.printf("GYRO_X : %d\n",sensingValue.GyX);
-  Serial.printf("GYRO_Y : %d\n",sensingValue.GyY);
-  Serial.printf("GYRO_Z : %d\n",sensingValue.GyZ);
-  Serial.printf("\n");
-  */
-
   // WIFI 공유기에 접속이 되지 않은 경우에는 연결시도
   if(WiFi.status()!=WL_CONNECTED){
     connect();
@@ -234,21 +265,40 @@ void loop() {
   // MQTT Broker 서버로 연결이 되어 있은 경우에는 연결시도
   if(!mqtt_client.connected()){
     mqtt_connect();
+  }  
+
+  int8_t check = whoAmI();
+  Serial.printf("WhoAmI : %x\n",check);
+  if(check!=0x68){
+    return;
   }
+  
+  wakeup();
+  measure();
+  
+  /**Serial.printf("ACCEL_X : %x\n",sensingValue.AcX);
+  Serial.printf("ACCEL_Y : %x\n",sensingValue.AcY);
+  Serial.printf("ACCEL_Z : %x\n",sensingValue.AcZ);
+  Serial.printf("TEMP : %f\n",(sensingValue.Temp)/340.00+36.53);
+  Serial.printf("GYRO_X : %x\n",sensingValue.GyX);
+  Serial.printf("GYRO_Y : %x\n",sensingValue.GyY);
+  Serial.printf("GYRO_Z : %x\n",sensingValue.GyZ);
+  Serial.printf("\n");*/
+  
+  // callibration값에서 측정된 가속도 값을 차감
+  sensingValue.AcX = (sensingValue.AcX-callibrationValue.AcX);
+  sensingValue.AcY = (sensingValue.AcY-callibrationValue.AcY);
+  sensingValue.AcZ = (sensingValue.AcZ-callibrationValue.AcZ);
 
   double gAcX = ((double)sensingValue.AcX/(double)G_UNIT);
   double gAcY = ((double)sensingValue.AcY/(double)G_UNIT);
   double gAcZ = ((double)sensingValue.AcZ/(double)G_UNIT);
   
   // MQTT Broker에 메시지 발행
-  char msg[80];
-  memset(msg,0x00,sizeof(msg));
-  sprintf(msg,"{\"vib\":[%lf,%lf,%lf]}", gAcX, gAcY, gAcZ);
+  sprintf(msg,"{\"status\":%x,\"vib\":[%lf,%lf,%lf]}", check, gAcX, gAcY, gAcZ);
   
   Serial.printf(msg);
   Serial.printf("\n");
   mqtt_client.publish("mpu6050",msg);
   Serial.println("Message is published.");
-
-  delay(20); // 0.01초 대기
 }
